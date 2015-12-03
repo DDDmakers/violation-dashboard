51d4c2b3b69200d0452322507654b8c302293cf6
DDDmakers <zsfinley@syr.edu>
2015-12-02 22:34:03 -0500

    Update app.R

--- app.R
+++ app.R
@@ -4,19 +4,43 @@ library(shiny)
 library(RCurl)
 
 url <- "https://github.com/DDDmakers/violation-dashboard/blob/master/Violation%20Report%20ALL.csv"
+url2 <- "https://github.com/DDDmakers/violation-dashboard/blob/master/all%20data%20by%20property.csv"
 dat.temp <- getURL( url, ssl.verifypeer = FALSE )
+dat.temp2 <- getURL( url2, ssl.verifypeer = FALSE )
 all.violations <- read.csv( textConnection( dat.temp ), stringsAsFactors=FALSE )  
+all.properties <- read.csv( textConnection( dat.temp ), stringsAsFactors=FALSE )  
 
 rm( url )
+rm( url2 )
 rm( dat.temp )
+rm( dat.temp2 )
 
-nrow(all.violations)
 
-##this generates an error that the Excel does not exists
-#all.violations <- read.xlsx("violation-dashboard/Violation Report ALL.xlsx")
+ui <- fluidPage(
+  headerPanel('Violation Dashboard Test'),
+  sidebarPanel(
+    selectInput('xcol', 'X Variable', names(all.properties)),
+    selectInput('ycol', 'Y Variable', names(all.properties),
+      selected = names(all.properties)[[2]]),
+  ),
+  mainPanel(
+    plotOutput('plot1')
+  )
+)
 
-ui <- fluidPage()
+server <- function(input, output) {
 
-server <- function(input, output) {}
+  selectedData <- reactive({
+    all.properties[, c(input$xcol, input$ycol)]
+  })
+
+  output$plot1 <- renderPlot({
+    par(mar = c(5.1, 4.1, 0, 1))
+    plot(selectedData(),
+         col = "lightblue",
+         pch = 19)
+  })
+
+}
 
 shinyApp(ui = ui, server = server)
