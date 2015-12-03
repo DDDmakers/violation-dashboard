cf85d8e6bc48615acc5647a300d85aeed29b8391
DDDmakers <zsfinley@syr.edu>
2015-12-02 21:44:00 -0500

    Update app.R

--- app.R
+++ app.R
@@ -3,9 +3,13 @@
 library(shiny)
 library(openxlsx)
 
-all.violations <- read.xlsx("violation-dashboard/Violation Report ALL.xlsx")
+url <- "https://raw.githubusercontent.com/DDDmakers/violation-dashboard/master/"Violation%20Report%20ALL.xlsx"
+dat.temp <- getURL( url, ssl.verifypeer = FALSE )
+all.violations <- read.xlsx( textConnection( dat.temp ))  
 nrow(all.violations)
+
 ##this generates an error that the Excel does not exists
+#all.violations <- read.xlsx("violation-dashboard/Violation Report ALL.xlsx")
 
 ui <- fluidPage()
 
