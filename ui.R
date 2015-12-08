1016516c102c7d9ab86ca0dae28a715b539a0980
DDDmakers <zsfinley@syr.edu>
2015-12-08 17:21:35 -0500

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,6 +1,21 @@
+#include library() as necessary
 
 shinyUI(navbarPage("Violation Dashboard",
   tabPanel("Violation Closeout"),
+  #under this I can add the specific layout elements of this page for example
+  #sidebarLayout(
+  #    sidebarPanel(
+  #      radioButtons("plotType", "Plot type",
+  #        c("Scatter"="p", "Line"="l")
+  #      )
+  #    ),
+  #    mainPanel(
+  #      plotOutput("plot")
+  #    )
+  #  )
+  #),
+  #make sure to remove ), after Closeout"
+  #and make ), the very last elements of this list
   tabPanel("Violation Frequency"),
   tabPanel("Violation Location")
 ))
